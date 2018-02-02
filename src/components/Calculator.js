f58f29b5e2cc00662ee34b89672fab26bfce8ac2
laraanna <lara@laraannawagner.com>
2018-02-02 15:55:04 +0100

    Set up Calculator Component

---
+++ src/components/Calculator.js
@@ -0,0 +1,14 @@
+import React, {PureComponent} from 'react'
+
+class Calculator extends PureComponent {
+  render(){
+    return(
+      <div className="Calculator">
+        <h1> Calculator </h1>
+      </div>
+    )
+  }
+
+}
+
+export default Calculator
