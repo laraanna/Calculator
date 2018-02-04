b7b139af1e67fa59a28369bebf79abf1a4d1863d
laraanna <lara@laraannawagner.com>
2018-02-04 14:15:55 +0100

    Set up Clear Element Action

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,17 @@
 import React, {PureComponent} from 'react'
 import { connect } from 'react-redux';
 import addElement from '../actions/addElement'
+import clearElement from '../actions/clearElement'
 import {BUTTONS} from '../constants'
 import './Calculator.css'
 
 class Calculator extends PureComponent {
   addElement(element){
     this.props.addElement(element)
+  }
+
+  clearElement(){
+    this.props.clearElement()
   }
   render(){
     const { value } = this.props.buttons;
@@ -17,9 +22,17 @@ class Calculator extends PureComponent {
           <input type="text" value={value}  />
         </div>
         <div className="ButtonsContainer">
-        {BUTTONS.map((element, key) =>
-          <button className="Buttons" key={key} onClick={() => this.addElement(element)}>{element}</button>
-        )}
+        {BUTTONS.map((element, key) => {
+          if(element === "C"){
+            return(
+              <button className="Buttons" key={key} onClick={() => this.clearElement(element)}>{element}</button>
+            )
+          } else {
+            return(
+              <button className="Buttons" key={key} onClick={() => this.addElement(element)}>{element}</button>
+            )
+          }
+        })}
         </div>
       </div>
     )
@@ -28,6 +41,6 @@ class Calculator extends PureComponent {
 }
 
 const mapStateToProps = ({ buttons }) => ({ buttons })
-const mapDispatchToProps = {addElement: addElement}
+const mapDispatchToProps = {addElement: addElement, clearElement: clearElement}
 
 export default connect(mapStateToProps,mapDispatchToProps)(Calculator)
--- src/reducers/buttons.js
+++ src/reducers/buttons.js
@@ -1,4 +1,5 @@
 import {ADD_ELEMENT} from '../actions/addElement'
+import {CLEAR_ELEMENT} from '../actions/clearElement'
 
 const initialState = {
   value: 0
@@ -10,6 +11,10 @@ export default (state=initialState, {type,payload} = {}) => {
       return {
         ...state,
         value: state.value ===  0 ? (payload) : (state.value + payload)
+      }
+    case CLEAR_ELEMENT:
+      return {
+        ...state, value: 0
       }
 
     default :
