3103a4714e1eaf7859c6b5a10ae33f2d6700f481
laraanna <lara@laraannawagner.com>
2018-02-04 12:50:24 +0100

    Create Button Constants and render

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,10 +1,20 @@
 import React, {PureComponent} from 'react'
+import {BUTTONS} from '../constants'
 
 class Calculator extends PureComponent {
   render(){
     return(
       <div className="Calculator">
         <h1> Calculator </h1>
+        <div className="Value">
+          <input type="text"  />
+        </div>
+        <div className="ButtonsContainer">
+        {BUTTONS.map((button, key) =>
+          <button className="Buttons" key={key}>{button}</button>
+        )}
+
+        </div>
       </div>
     )
   }
